26bfdfa2796f7805bc538c5e8baf61d23957b9f5
Arjun VB <arjunvb22@gmail.com>
2026-07-15 03:16:58 +0530

    Fix missing dj_database_url import

--- luxury_car_hub/settings.py
+++ luxury_car_hub/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from decouple import config
 from pathlib import Path
-from dj_database_url import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
